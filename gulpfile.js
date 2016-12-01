662d7548ffa7daeba93319dcaeaa81dd3dd4619b
Narottam Dattatrey <dattatrey93@gmail.com>
2016-12-02 00:08:23 +0530

    build: minifed build file into single file

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,13 @@
 //Dependencies
 var gulp = require('gulp');
 var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
 
 //methods
 //gulp-uglify minified the javascript file.
 gulp.task('default', function() {
     gulp.src('development/personal-website/**/*.js')
         .pipe(uglify())
+        .pipe(concat('app.min.js'))
         .pipe(gulp.dest('dist'));
 });
