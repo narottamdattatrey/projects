bce12f74a9c9584cd25ded95b1ce7880caaad5c0
Narottam Dattatrey <dattatrey93@gmail.com>
2016-12-03 17:37:43 +0530

    build: sass compilation

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,8 @@
 var gulp = require('gulp');
 var uglify = require('gulp-uglify');
 var concat = require('gulp-concat');
-var webserver = require('gulp-webserver');
+// var webserver = require('gulp-webserver');
+var sass = require('gulp-sass');
 
 //methods 
 //gulp-uglify uglify the files.
@@ -11,18 +12,24 @@ gulp.task('scripts', function() {
     return gulp.src('development/personal-website/**/*.js')
         .pipe(uglify())
         .pipe(concat('app.min.js'))
-        .pipe(gulp.dest('dist'));
+        .pipe(gulp.dest('dist/js'));
 });
 //always add return to above type like functions
 
 gulp.task('watch', function(){
-	gulp.watch('development/personal-website/**/*.js', ['scripts']);
+	gulp.watch('development/**/*.js', ['scripts']);
 });
-gulp.task('webserver', function(){
-	gulp.src('app')
-	.pipe(webserver({
-		livereload: true,
-		directoryListing: true,
-		open: true
-	}));
+// gulp.task('webserver', function(){
+// 	gulp.src('app')
+// 	.pipe(webserver({
+// 		livereload: true,
+// 		directoryListing: true,
+// 		open: true
+// 	}));
+// });
+
+gulp.task('sass', function(){
+	return gulp.src('development/**/*.scss')	
+	.pipe(concat('vender.min.css'))
+	.pipe(gulp.dest('dist/css'));
 });
