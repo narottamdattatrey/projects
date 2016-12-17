bd7fa37e991caecfb0f2a8017b29f6e0b5b3ad02
Narottam Dattatrey <dattatrey93@gmail.com>
2016-12-17 11:37:17 +0530

    firebase setup

---
+++ index.js
@@ -0,0 +1,12 @@
+<script src="https://www.gstatic.com/firebasejs/3.6.4/firebase.js"></script>
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAtvYUMSl9__hisM9jrV2f2u2lYjzROfvg",
+    authDomain: "projects-846ba.firebaseapp.com",
+    databaseURL: "https://projects-846ba.firebaseio.com",
+    storageBucket: "projects-846ba.appspot.com",
+    messagingSenderId: "503554390859"
+  };
+  firebase.initializeApp(config);
+</script>
