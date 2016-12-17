2820756d5271eed0451b183653c0089c14405972
Narottam Dattatrey <dattatrey93@gmail.com>
2016-12-17 14:13:41 +0530

    #setup: firebase

---
+++ index.config.js
@@ -0,0 +1,10 @@
+(function() {
+    //srict mode for iife
+    'use strict';
+    var webapp = angular.module('project');
+
+    /** single page route configuration */
+    webapp.config($routerProvider, $http, $stateProvider) {
+
+    }
+})();
